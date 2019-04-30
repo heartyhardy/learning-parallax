25d7eee2bd98489d9382758e03287939692981d0
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-04-30 19:23:25 +0530

    Fixed a minor bug in Layer

--- src/components/hoc/layer/Layer.js
+++ src/components/hoc/layer/Layer.js
@@ -1,14 +1,20 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 const Layer = props => {
 
     const elLayer = useRef(null);
 
+    const [initialTop, setInitialTop] =useState(0);
+
+    useEffect(()=>{
+        const bbox = elLayer.current.getBoundingClientRect();
+        setInitialTop(prev => prev+ bbox.top);
+    },[])
+
     useEffect(() => {
         const bbox = elLayer.current.getBoundingClientRect();
-        elLayer.current.style.top = `${bbox.top * props.scrollPos/(props.layerIndex * 1000)}px`;
-
-    }, [props.scrollPos, props.layerIndex])
+        elLayer.current.style.top = `${ initialTop + (bbox.top * props.scrollPos/(props.layerIndex * 1000))}px`;
+    }, [props.scrollPos, props.layerIndex, initialTop])
 
     return (
         <div
